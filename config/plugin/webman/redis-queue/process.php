ec634afc1acad325c46d92de499a4d1847e13631
fdj <fdj@kuryun.cn>
2023-06-17 22:33:15 +0800

    update process count

--- config/plugin/webman/redis-queue/process.php
+++ config/plugin/webman/redis-queue/process.php
@@ -1,8 +1,10 @@
 <?php
+
+
 return [
     'consumer'  => [
         'handler'     => Webman\RedisQueue\Process\Consumer::class,
-        'count'       => cpu_count() * 5, // 可以设置多进程同时消费
+        'count'       => cpu_count() * 6, // 可以设置多进程同时消费
         'constructor' => [
             // 消费者类目录
             'consumer_dir' => app_path() . '/queue/redis'
