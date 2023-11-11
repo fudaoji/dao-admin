099731c7d888f43941174e462341892f0c55864a
fdj <fdj@kuryun.cn>
2023-11-11 17:56:23 +0800

    update

---
+++ extend/EasyOpen/Tencent/Request/Mp/WxaGetEffectiveJumpDomain.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Script Name: WxaGetEffectiveServerDomain.php
+ * Create: 2023/11/11 17:46
+ * Description:
+ * Author: fudaoji<fdj@kuryun.cn>
+ */
+
+namespace EasyOpen\Tencent\Request\Mp;
+
+
+class WxaGetEffectiveJumpDomain implements RequestInterface
+{
+    private $url = "https://api.weixin.qq.com/wxa/get_effective_webviewdomain";
+    private $getParams = array();
+    private $postParams = array('{}');
+
+    /**
+     * 获取请求url
+     * @author fudaoji<fdj@kuryun.cn>
+     */
+    public function getUrl(){
+        return $this->url;
+    }
+
+    /**
+     * 设置请求地址
+     * @param string $url
+     * @author fudaoji<fdj@kuryun.cn>
+     */
+    public function setUrl($url) {
+        $this->url = $url;
+    }
+
+    /**
+     * get请求参数
+     * @author fudaoji<fdj@kuryun.cn>
+     */
+    public function getParams() {
+        return $this->getParams;
+    }
+
+    /**
+     * post请求参数
+     * @author fudaoji<fdj@kuryun.cn>
+     */
+    public function postParams() {
+        return $this->postParams;
+    }
+}
